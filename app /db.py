39355224514c92e2cdab28427fe44b7ee64b33c6
Enthusiasm-c <devragrouphk@gmail.com>
2025-04-27 13:14:53 +0800

    Update db.py

--- app /db.py
+++ app /db.py
@@ -1,26 +1,9 @@
-__all__ = ["settings"]
+__all__ = ["engine", "SessionLocal", "Base"]
 
-from pydantic import BaseSettings
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from .config import settings
 
-class Settings(BaseSettings):
-    """App config
-
-    Variables:
-        telegram_token: str    # Токен Telegram-бота
-        db_url: str      # URI для подключения к PostgreSQL
-        gpt_ocr_url: str
-        gpt_parsing_url: str
-        fuzzy_threshold: float
-        syrve_url: str
-        syrve_token: str|None
-    """
-
-    telegram_token: str = "TELEGRAM_TOKEN"
-    db_url: str = "postgresql+asyncpg://user:pass@localhost/invoices_db"
-    gpt_ocr_url: str = "http://localhost:8001/gpt-ocr"        # Dummy/mock
-    gpt_parsing_url: str = "http://localhost:8001/gpt-parse"
-    fuzzy_threshold: float = 0.9
-    syrve_url: str = "http://localhost:8080/syrve"
-    syrve_token: str | None = None  # TODO: Заполнить реальный ключ
-
-settings = Settings()
+engine = create_async_engine(settings.db_url, echo=False, future=True)
+SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+Base = declarative_base()
