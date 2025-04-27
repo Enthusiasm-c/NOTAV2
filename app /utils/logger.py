0b1f5e255aac6fd5c8bca1ea00d2ccbaa7d530ce
Enthusiasm-c <devragrouphk@gmail.com>
2025-04-27 15:52:28 +0800

    Update logger.py

--- app /utils/logger.py
+++ app /utils/logger.py
@@ -1,12 +1,15 @@
-__all__ = ["log"]
+import structlog
+import sys
 
-import logging
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s %(levelname)s: %(message)s"
+logger = structlog.configure(
+    processors=[
+        structlog.processors.TimeStamper(fmt="iso"),
+        structlog.processors.JSONRenderer(),
+    ],
+    logger_factory=structlog.stdlib.LoggerFactory(),
+    wrapper_class=structlog.stdlib.BoundLogger,
+    cache_logger_on_first_use=True,
 )
 
-def log(message: str):
-    """Простой логгер"""
-    logging.info(message)
+if __name__ == "__main__":
+    logger.info("Logger test", foo="bar")
