8eb26e782057f3d1a25777922509b2974c3cfb6e
Enthusiasm-c <devragrouphk@gmail.com>
2025-04-27 15:57:15 +0800

    Update test_fuzzy_match.py

--- tests/test_fuzzy_match.py
+++ tests/test_fuzzy_match.py
@@ -3,8 +3,7 @@ import pytest
 from app.routers.fuzzy_match import fuzzy_match_product
 from app.db import SessionLocal, Base, engine
 from app.models.product import Product
-import sys, os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 
 @pytest.mark.asyncio
 async def test_fuzzy_match_product():
