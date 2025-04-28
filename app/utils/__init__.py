dfb12814c83bc19c2da406f399b55057bd9fe5fc
Enthusiasm-c <devragrouphk@gmail.com>
2025-04-28 20:35:36 +0800

    Update __init__.py

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,1 +1,9 @@
+"""
+Utils package for Nota V2.
 
+Contains helper modules and utility functions.
+"""
+
+# This empty __init__.py file is necessary for Python to recognize
+# the utils directory as a package, allowing imports like:
+# from app.utils.unit_converter import normalize_unit
